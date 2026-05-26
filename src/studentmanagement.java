fde0207437251aaa9bf5a87d3fb7f4a0d95235a6
Ofentse06-dev <matheofentse322@gmail.com>
2026-05-26 18:58:36 +0200

    Create studentmanagement.java

---
+++ src/studentmanagement.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class StudentManager {
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void showStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students available.");
+        } else {
+            for (Student s : students) {
+                s.displayStudent();
+            }
+        }
+    }
+
+    public void searchStudent(int id) {
+        for (Student s : students) {
+            if (s.getId() == id) {
+                System.out.println("Student found:");
+                s.displayStudent();
+                return;
+            }
+        }
+        System.out.println("Student not found.");
+    }
+}
